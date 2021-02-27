3fde59df39232a5c5fb9e94a60c1533c7080decf
Elvis Landi <elandi13@yahoo.com>
2021-02-27 11:12:30 -0500

    Deliverable 3

--- src/components/App.js
+++ src/components/App.js
@@ -1,16 +1,41 @@
-import React from "react";
+import React, {useEffect, useState} from "react";
+// import MoreButton from "./MoreButton";
 import SushiContainer from "./SushiContainer";
 import Table from "./Table";
 
 const API = "http://localhost:3001/sushis";
 
-function App() {
+  function App() {
+  const [isSushis, setIsSushis] = useState([])
+  const [sushiIndex, setSushiIndex] = useState(0);
+  useEffect (() => {
+    fetch(API)
+    .then(res => res.json())
+    .then((sushiObj) => {
+      const updatedSushis = sushiObj.map((sushi) => {
+        return { ...sushi}; //3 dots means current sushi
+  })
+    setIsSushis(updatedSushis);
+  })
+  }, [])
+
+  function handleClick() {
+    // console.log("Clicked")
+    setSushiIndex((sushiIndex) => (sushiIndex + 4) % isSushis.length);
+    // console.log(isSushis.length)
+  }
+
+  const displaySushi = isSushis.slice(sushiIndex, sushiIndex + 4);
+
   return (
     <div className="app">
-      <SushiContainer />
+      <SushiContainer sushis={displaySushi} 
+      onClickMore ={handleClick} />
       <Table />
     </div>
   );
 }
 
 export default App;
+
+  
--- src/components/SushiContainer.js
+++ src/components/SushiContainer.js
@@ -1,13 +1,20 @@
 import React from "react";
 import MoreButton from "./MoreButton";
+import Sushi from "./Sushi"
 
-function SushiContainer(props) {
+
+function SushiContainer({ sushis, onClickMore}) {
+  const sushiComponents = sushis.map((sushi) => (
+    <Sushi key={sushi.id} name={sushi.name} image={sushi.img_url} price= {sushi.price}/>
+  ))
+
   return (
     <div className="belt">
-      {/* Render Sushi components here! */}
-      <MoreButton />
+      {sushiComponents}
+      <MoreButton onClickMore={onClickMore} />
     </div>
   );
 }
 
 export default SushiContainer;
+
